04ec17e7600637c0554c09dba541dc8e2e36e4ba
Nazmul Huda <nznazmulhuda04@gmail.com>
2024-10-10 12:31:48 +0600

    navbar added

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,64 @@
+import styles from "./style.module.css";
+import subtract from "./subtract.svg";
+
+export const Navbar = () => {
+	const links = [
+		{
+			path: "#",
+			title: "Home",
+		},
+		{
+			path: "#",
+			title: "About Me",
+		},
+		{
+			path: "#",
+			title: "Services",
+		},
+		{
+			path: "#",
+			title: "Projects",
+		},
+		{
+			path: "#",
+			title: "Testimonials",
+		},
+		{
+			path: "#",
+			title: "Contact",
+		},
+	];
+
+	return (
+		<nav className={styles["about-me"]}>
+			<div className={styles.group}>
+				{/* logo part */}
+				<a href="#" className={styles.logo}>
+					<img className={styles.subtract} alt="Subtract" src={subtract} />
+
+					<p className={styles.mumair}>
+						<span className={styles["text-wrapper"]}>M</span>
+						<span className={styles.span}>umair </span>
+					</p>
+				</a>
+
+				{/* links */}
+				<ul className={styles.frame}>
+					{links.map((link, index) => (
+						<li key={index} className={styles["div-wrapper"]}>
+							<a href={link.path} className={styles["text-wrapper-2"]}>
+								{link.title}
+							</a>
+						</li>
+					))}
+				</ul>
+
+				<a href="#" className={styles["overlap-group-wrapper"]}>
+					<button href="#" className={styles["overlap-group"]}>
+						<div className={styles["text-wrapper-3"]}>Download CV</div>
+					</button>
+				</a>
+			</div>
+		</nav>
+	);
+};
